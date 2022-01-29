8409b1594a831aab4861a021cc071b08979ff5ea
tomasz t <tomasztaras@outlook.com>
2022-01-29 21:21:43 +0100

    added search bar based on nominatim geocoder - #35

--- src/map.js
+++ src/map.js
@@ -102,6 +102,53 @@ let geolocate = new maplibregl.GeolocateControl({
 });
 map.addControl(geolocate, controlsLocation);
 
+var geocoder_api = {
+    forwardGeocode: async (config) => {
+        const features = [];
+        try {
+            let request =
+                'https://nominatim.openstreetmap.org/search?q=' +
+                config.query +
+                '&format=geojson&polygon_geojson=1&addressdetails=1';
+            const response = await fetch(request);
+            const geojson = await response.json();
+            for (let feature of geojson.features) {
+                let center = [
+                    feature.bbox[0] +
+                    (feature.bbox[2] - feature.bbox[0]) / 2,
+                    feature.bbox[1] +
+                    (feature.bbox[3] - feature.bbox[1]) / 2
+                ];
+                let point = {
+                    type: 'Feature',
+                    geometry: {
+                        type: 'Point',
+                        coordinates: center
+                    },
+                    place_name: feature.properties.display_name,
+                    properties: feature.properties,
+                    text: feature.properties.display_name,
+                    place_type: ['place'],
+                    center: center
+                };
+                features.push(point);
+            }
+        } catch (e) {
+            console.error(`Failed to forwardGeocode with error: ${e}`);
+        }
+
+        return {
+            features: features
+        };
+    }
+};
+map.addControl(
+    new MaplibreGeocoder(geocoder_api, {
+        maplibregl: maplibregl
+    }),
+    'top-right'
+);
+
 console.log('Loading icon...');
 map.loadImage('./src/img/marker-image_50.png', (error, image) => {
     if (error) throw error;
