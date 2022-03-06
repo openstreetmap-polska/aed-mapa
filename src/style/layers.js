f71e96195509b514249936e7a36bb1cfdf3e1975
Maciej <bandurr@gmail.com>
2022-03-06 19:41:54 +0100

    icon-size -> 0.33

--- src/style/layers.js
+++ src/style/layers.js
@@ -39,7 +39,7 @@ let layers = {
       "type": "symbol",
       "source": "aed-locations",
       "filter": ["!", ["has", "point_count"]],
-      "layout": {"icon-image": "aed-default", "icon-size": 0.5}
+      "layout": {"icon-image": "aed-default", "icon-size": 0.33}
     },
     {
       "id": "clustered-circle",
